2966d864446228490dd9a3e446ead12edb12ae4f
PIYUSH GARG <piyushgargxd@gmail.com>
2026-05-28 11:47:22 +0530

    Add files via upload

---
+++ week0/faces.py
@@ -0,0 +1,4 @@
+text = input("Enter the text: ")
+text = text.replace(":)", "🙂")
+text = text.replace(":(", "🙁")
+print(f"Your text with emoticons replaced is: {text}")
---
+++ week0/indoor.py
@@ -0,0 +1,4 @@
+text = input("Enter anything you want in lowercases: ")
+text = text.lower()
+
+print(f"Your text in lowercases is: {text}")
